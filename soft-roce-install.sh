7d009bacdfc1f085ba639d54a8be9cf93a85b347
Haggai Eran <haggaie@mellanox.com>
2018-01-17 14:32:55 +0200

    provision script fixes
    Update apt once.
    Use enp0s3 instead of enp3s0.

--- soft-roce-install.sh
+++ soft-roce-install.sh
@@ -14,21 +14,26 @@
 #     pkg-config \
 #     valgrind
 
+# install kmod from ppa to work around issue https://bugs.launchpad.net/ubuntu/+source/kmod/+bug/1693503
+sudo add-apt-repository ppa:bdrung/ppa
+# librxe is not inbox in Ubuntu yet
+sudo add-apt-repository ppa:linux-rdma/rdma-core-daily
+
+sudo apt-get update
+
 # The RDMA stack and SoftRoCE require the generic kernel (not virtual kernel)
 sudo apt-get install -y linux-generic
 sudo apt-get autoremove --purge linux-virtual
 
 # install kmod from ppa to work around issue https://bugs.launchpad.net/ubuntu/+source/kmod/+bug/1693503
-sudo add-apt-repository ppa:bdrung/ppa
-sudo apt-get update && sudo apt-get install -y kmod
+sudo sudo apt-get install -y kmod
 
 # librxe is not inbox in Ubuntu yet
-sudo add-apt-repository ppa:linux-rdma/rdma-core-daily
-sudo apt-get update && sudo apt-get install -y rdma-core
+sudo sudo apt-get install -y rdma-core
 
 # Generic RDMA utilities
 sudo apt-get install -y ibverbs-utils perftest
 
 # enable rxe
 sudo rxe_cfg start
-sudo rxe_cfg add enp3s0
+sudo rxe_cfg add enp0s3
