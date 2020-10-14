57870301aa4a95c23e2aaac30272ca8355495d01
Haggai Eran <haggai.eran@gmail.com>
2020-10-14 18:48:55 +0300

    packer: no need for daily rdma-core and custom built sockperf
    Focal versions are fine.

--- build/packer/scripts/setup-vm.sh
+++ build/packer/scripts/setup-vm.sh
@@ -2,25 +2,6 @@
 
 ### Install Soft RoCE
 
-# # for building rdma-core
-# 
-# apt-get update && apt-get install -y \
-#     build-essential \
-#     cmake \
-#     gcc \
-#     git \
-#     libudev-dev \
-#     libnl-3-dev \
-#     libnl-route-3-dev \
-#     ninja-build \
-#     pkg-config \
-#     valgrind
-
-# librxe is not inbox in Ubuntu yet
-add-apt-repository ppa:linux-rdma/rdma-core-daily
-# rdma-experiment ppa - sockperf
-add-apt-repository ppa:haggai-eran/rdma-experiment
-
 apt-get update
 
 # install ifupdown for Vagrant
@@ -30,14 +11,8 @@ apt-get install -y ifupdown
 apt-get install -y linux-generic
 apt-get autoremove -y --purge linux-virtual
 
-# install latest kmod since previous version conflicts with rdma-core
-apt-get install -y kmod
-
-# librxe is not inbox in Ubuntu yet
-apt-get install -y rdma-core
-
-# Generic RDMA utilities
-apt-get install -y ibverbs-utils perftest rdmacm-utils
+# rdma-core and utilities
+apt-get install -y rdma-core ibverbs-utils perftest rdmacm-utils
 
 # enable rxe
 echo rdma_rxe > /etc/modules-load.d/rxe.conf
